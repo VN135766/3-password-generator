676fe859f2ab2c4e9440e32f5a907d132004e6ee
Victor Nunez <VN135766@gmail.com>
2022-06-22 19:43:43 -0400

    added arrays containing characters and prompt command

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,12 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var passwordLength;
+const uppercase = ["A", "B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+// console.log(uppercase.length)
+const lowercase = uppercase.map(name => name.toLowerCase());
+// console.log(lowercase)
+const numbers = [0,1,2,3,4,5,6,7,8,9,0]
+const special = ["!", "'", '"', "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".","/",";",":","<",">","=","?","@","[","]","\ ","{","}", "|","~","`"]
 
 // Write password to the #password input
 function writePassword() {
@@ -12,23 +19,36 @@ function writePassword() {
 //PSEUDO CODE
 
 //Click button to generate password
+function generatePassword() {
+    prompt()
+  }
+  
+  generatePassword();
 
-//Responses will be stored in object called userResponses
+//generatePassword function will begin.
+
+//Prompts will display using prompt().  Responses will be stored in object called userResponses
 
 //Prompt #1 = How many characters would you like for your password to have?  Must have a minimum of 8 and a maximum of 128.
 //variable = passwordLength
 
 //Prompt #2 = Would you like for your password to have uppercase letters?
 //array uppercase = [A, B, C, D, ... X, Y, Z]
+//if yes, concat with array password = []
 
 //Prompt #3 = Would you like for your password to have lowercase letters?
 //array lowercase = [a, b, c, d, ... x, y, z]
+//if yes, concat with array password = []
 
 //Prompt #4 = Would you like for your password to have numbers?
 //array numbers = [1, 2, 3, 4 ... 8, 9, 0]
+//if yes, concat with array password = []
 
 //Prompt #5 = Would you like for your password to have special characters?
 //array special = [!, ", #, $, %, &, ', ( ... +, -, ^]
+//if yes, concat with array password = []
 
+//use to loop to pull a single character from array password "i" or "passwordLength" times.
+//ensure that every pull is random using math.random().
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
